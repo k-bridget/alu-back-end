fba5ea1fb7bc4df7b432c6430f5cf08b8498f2e6
k-bridget <k.bridget@alustudent.com>
2025-06-27 09:27:09 +0000

    first python script using REST API

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" ToDo list """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    employee_url = "https://jsonplaceholder\
+.typicode.com/users/{}".format(employee_id)
+    todo_url = "https://jsonplaceholder\
+.typicode.com/users/{}/todos".format(employee_id)
+
+    employee_data = requests.get(employee_url).json()
+    employee_name = employee_data['name']
+
+    todo_data = requests.get(todo_url).json()
+
+    num_completed_tasks = 0
+    for task in todo_data:
+        if task['completed']:
+            num_completed_tasks += 1
+
+    total_num_tasks = len(todo_data)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_completed_tasks, total_num_tasks))
+
+    for task in todo_data:
+        if task['completed']:
+            print("\t {}".format(task['title']))
